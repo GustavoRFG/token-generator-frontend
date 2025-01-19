ab888799c4803597f49b6aa3256288a1b7dc0d81
Gustavo <guxgomes@gmail.com>
2025-01-19 00:33:51 -0300

    Second commit

---
+++ types.d.ts
@@ -0,0 +1,5 @@
+// types.d.ts
+/// <reference types="@types/babel__core" />
+/// <reference types="@types/babel__generator" />
+/// <reference types="@types/babel__template" />
+/// <reference types="@types/babel__traverse" />
